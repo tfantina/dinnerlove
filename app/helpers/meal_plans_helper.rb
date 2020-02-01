05f4c634f06ef497591497859a960d1878b5c732
tfantina <tfantina@gmail.com>
2020-02-01 09:19:50 -0600

    Fixes button (active week is no longer a week)

--- app/helpers/meal_plans_helper.rb
+++ app/helpers/meal_plans_helper.rb
@@ -2,9 +2,7 @@ module MealPlansHelper
     def active_week(current_plan, week)
       puts current_plan[:weekof] == week.weekof
       if current_plan[:weekof] == week.weekof
-        link_to current_plan[:weekof],
-            meal_plan_path(current_plan[:id]),
-            class: "button button-show-active button-log-radius"
+            content_tag(:a, current_plan[:weekof], class: "button button-show-active button-log-radius")
       else
       link_to current_plan[:weekof],
             meal_plan_path(current_plan[:id]),
