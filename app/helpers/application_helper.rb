37f385298884dc8f43b9bbacdbe2462ff7ba3514
tfantina <tfantina@gmail.com>
2020-02-08 09:26:53 -0600

    Fixes issues with the footer, and adds a clever footer helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
+  def link_builder(link)
+  
+    case link
+    when :shuffle
+      link_to "Shuffle", dinners_path, class: "button button-create button-log-radius"
+    when :add_dinner
+      link_to "Add Dinner", new_dinner_path, class: "button button-create button-log-radius"
+    end
+  end
 
 end
