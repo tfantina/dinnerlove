c893724ad368cb3488e44ef31986981ee578451f
tfantina <tfantina@gmail.com>
2020-03-23 07:04:47 -0600

    Adds digital ocean s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon # :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 
- 
+
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
